fbb81c18a7f85748bf8ae455cd73f7b6928b4a7c
DaianaRodriguez <daiabelcastro@hotmail.com>
2024-07-01 13:42:27 -0300

    release
    hey

--- mail.php
+++ mail.php
@@ -56,4 +56,4 @@ $headers = "MIME-Version: 1.0" . PHP_EOL .
 'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
 'Reply-To: '.$admin_email.'' . PHP_EOL;
 
-mail($admin_email, adopt($form_subject), $message, $headers );
+mail('daiabelcastro@hotmail.com', adopt($form_subject), $message, $headers );
